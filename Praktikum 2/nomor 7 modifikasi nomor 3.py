36a301dafb304e5d58a342ee9461d2f4ef3dbaf6
FarrasAji <Farrasaji.rpl2.16@gmail.com>
2020-10-31 20:20:34 +0700

    Add files via upload

---
+++ Praktikum 2/nomor 7 modifikasi nomor 3.py
@@ -0,0 +1,7 @@
+def luasSegitigakedua(alas1,tinggi1):
+    luas1 = alas1 * tinggi1 / 2
+    print('Luas segitiga kedua dengan alas',alas1 ,'dan tinggi',tinggi1,'adalah',luas1)
+
+alas1 = 15
+tinggi1 = 45
+luasSegitigakedua(alas1,tinggi1)
